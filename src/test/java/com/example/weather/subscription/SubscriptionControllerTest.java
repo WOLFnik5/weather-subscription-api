a005043e9a27705a5379024ce2522310cb47ed17
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-08-29 00:40:06 +0200

    Annotate tests with Transactional

--- src/test/java/com/example/weather/subscription/SubscriptionControllerTest.java
+++ src/test/java/com/example/weather/subscription/SubscriptionControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class SubscriptionControllerTest {
 
     @Autowired
