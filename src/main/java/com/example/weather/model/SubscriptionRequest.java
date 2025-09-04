f73369fbe146aa4043305ebb65d96c436f55bfbd
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-09-04 03:07:56 +0200

    Add OpenAPI documentation

--- src/main/java/com/example/weather/model/SubscriptionRequest.java
+++ src/main/java/com/example/weather/model/SubscriptionRequest.java
@@ -5,15 +5,19 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Subscription request")
 public class SubscriptionRequest {
     @Email
     @NotBlank
+    @Schema(description = "Subscriber email")
     private String email;
 
     @NotBlank
+    @Schema(description = "City for weather updates")
     private String city;
 }
