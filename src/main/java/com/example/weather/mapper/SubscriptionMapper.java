21177f10a7b66069ba303365afb7f2e5dec475dc
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-09-10 16:09:38 +0200

    feat: import locale in mapper

--- src/main/java/com/example/weather/mapper/SubscriptionMapper.java
+++ src/main/java/com/example/weather/mapper/SubscriptionMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import java.util.Locale;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", imports = {Locale.class})
 public interface SubscriptionMapper {
 
     SubscriptionDto toDto(Subscription subscription);
