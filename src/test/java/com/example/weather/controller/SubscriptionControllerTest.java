9a5a3855b5763b791bece610b63b26693d8e6df1
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2025-09-15 19:22:16 +0200

    Use MockitoBean in subscription controller test

--- src/test/java/com/example/weather/controller/SubscriptionControllerTest.java
+++ src/test/java/com/example/weather/controller/SubscriptionControllerTest.java
@@ -11,7 +11,7 @@ import com.example.weather.mapper.SubscriptionMapperImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockitoBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -34,7 +34,7 @@ class SubscriptionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private SubscriptionService service;
 
     @Test
